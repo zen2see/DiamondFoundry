af8bb6087a4f0e64c0bf97f9ade3da59aff404bb
zen2see <zen2se@gmail.com>
2023-10-09 16:01:39 -0400

    PROGRESSION from base Diamond by adding FundMe contract

---
+++ src/facets/FundMeFacet.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {AppStorage, Modifiers} from "../libraries/LibAppStorage.sol";
+import {LibMeta} from "../../../shared/libraries/LibMeta.sol";
+import {LibConstants} from "../libraries/LibConstants.sol";
+import {LibPriceConverter} from "../libraries/LibPriceConverter.sol";
+error not_Owner();
+
+contract FundMeFacet is Modifiers {
+    using LibPriceConverter for uint256;
+
+    function fund() payable external {
+        require(msg.value.getConversionRate() >= LibConstants.MINIMUM_USD, "Didn't send enough ETH");
+        s.addressToAmountFunded[msg.sender] += msg.value;
+        s.funders.push(msg.sender);
+    }
+
+    function getVersion() external view returns (uint256) {
+
+    }
+
+    function withdraw() external onlyOwner {
+        for (uint256 funderIndex = 0; funderIndex < s.funders.length; funderIndex++) {
+            address funder = s.funders[funderIndex];
+            s.addressToAmountFunded[funder] = 0;
+        }
+        s.funders = new address[](0);
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed"); 
+    }
+
+    fallback() external payable {
+        this.fund();
+    }
+
+    receive() external payable {
+        this.fund();
+    }
+
+}
--- src/libraries/LibAppStorage.sol
+++ src/libraries/LibAppStorage.sol
@@ -1,25 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
+
 import {LibDiamond} from "../../../shared/libraries/LibDiamond.sol";
 import {LibMeta} from "../../../shared/libraries/LibMeta.sol";
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 // import {ILink} from "../../../shared/interfaces/ILink.sol";
 // import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 
 // uint256 constant NUMERIC = 6;
 
-struct DiamondFoundry {
-    string name;
-    uint256 randomNumber;
-    address owner;
-    uint8 status; // 0 == , 1 == VRF_PENDING,
-    bool locked;
-    address escrow; //The escrow address thismanages.
-}
+// struct DiamondFoundry {
+//     uint256 randomNumber;
+//     uint8 status; // 0 == , 1 == VRF_PENDING,
+//     bool locked;
+//     address escrow; //The escrow address thismanages.
+// }
 
 struct AppStorage {
     // mapping(address => uint32[]) ownerTokenIds;
     // mapping(address => mapping(uint256 => uint256)) ownerTokenIdIndexes;
-    string name;
+    mapping(address => uint256) addressToAmountFunded;
+    address[] funders;
+    address i_owner;
     // string symbol;
     // //Addresses
     // address aContract;
